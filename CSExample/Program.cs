32fa9da5ee739051384c9c6c9f9671f9a21492a7
Thong Nguyn <thongdatng201@gmail.com>
2025-01-12 14:48:35 +0700

    Day 1: The ternary conditional operator in branch and loop statements.

--- CSExample/Program.cs
+++ CSExample/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace CSExample
+﻿namespace CS01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Tim GTLN cua 2 so a va b
+
+            //float a, b, max;
+
+            //a = float.Parse(Console.ReadLine());
+            //b = float.Parse(Console.ReadLine());
+
+
+            //if (a > b)
+            //{
+            //    max = a;
+            //}
+            //else
+            //{
+            //    max = b;
+            //}
+            /* => co the toi uu bang TOAN TU 3 NGOI*/
+
+            //    max = (a > b) ? a : b;
+            //Console.WriteLine("So lon nhat la {0}", max);
+
+            // Tim GTLN cua 3 so a, b, c
+            float a, b, c, max;
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    if ( a > c)
+            //    {
+            //        max = a;
+            //    }    
+            //    else
+            //    {
+            //        max = c;
+            //    }    
+            //}    
+            //else
+            //{
+            //    if( b > c)
+            //    {
+            //        max = b;
+            //    }    
+            //    else
+            //    {
+            //        max = c;
+            //    }    
+            //}
+
+            /* => co the toi uu bang TOAN TU 3 NGOI*/
+            max = (a > b) ? (a > c) ? a : c : (b > c) ? b : c;
+            Console.WriteLine("So lon nhat la {0}", max);
         }
     }
 }
