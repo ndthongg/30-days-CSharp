bac2251a26beff24bc7e856a44e8207a128b7c5b
Thong Nguyn <thongdatng201@gmail.com>
2025-01-14 22:19:25 +0700

    Edit a little

--- CSExample/Program.cs
+++ CSExample/Program.cs
@@ -2,9 +2,49 @@
 {
     internal class Program
     {
+        // Branch with switch case and go to statements.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a, b;
+            Console.Write("Nhap a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Chon phep toan: ");
+            Console.WriteLine("1. Cong");
+            Console.WriteLine("2. Tru");
+            Console.WriteLine("3. Nhan");
+            Console.WriteLine("4. Chia");
+
+            char kiTu;
+            C1:
+            kiTu = Console.ReadKey().KeyChar;
+            Console.WriteLine();    
+
+            switch (kiTu)
+            {
+                case '1':
+                    Console.WriteLine($"Tong la: {a + b}");
+                    break;
+                case '2':
+                    Console.WriteLine($"Hieu la: {a - b}");
+                    break;
+                case '3':
+                    Console.WriteLine($"Tich la: {a * b}");
+                    break;
+                case '4':
+                    Console.WriteLine($"Thuong la: {a / b}");
+                    break;
+                default:
+                    Console.WriteLine("Ban nhap sai. Hay nhap lai");
+                    goto C1;
+                    break;
+            }    
+
+            Console.Title = "CSExample";
+
         }
     }
 }
