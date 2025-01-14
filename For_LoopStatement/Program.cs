11ddb5e661097e1b18919213e92adb2673f27b1c
Thong Nguyn <thongdatng201@gmail.com>
2025-01-14 22:17:44 +0700

    CS01 13/1/25: For_LoopStatements

---
+++ For_LoopStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LoopStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // For loop statement
+            /*
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"3 x {i} = {3 * i}");
+            }
+            */
+
+
+            // While loop statement
+            /*
+            int i = 1;
+            while(i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }    
+            */
+
+
+            // Do-While loop statement
+            /**/
+            int i = 1;
+            do
+            {
+                Console.WriteLine($" 3 x {i} = {3 * i}");
+                i++;
+            } while (i <= 10);
+
+        }
+    }
+}
