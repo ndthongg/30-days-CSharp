71b6e1223b70a52d28ff07da2e621e75ab82abdf
Thong Nguyn <thongdatng201@gmail.com>
2025-01-15 21:10:58 +0700

    Add BT4

---
+++ BT4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BT4
+{
+    internal class Program
+    {
+        /*
+         
+            Bài 8. Viết chương trình cho nhập vào số phải là số chính phương, 
+                    xuất số vừa nhập ra màn hình.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao mot so nguyen: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (IsPerfectSquare(n))
+            {
+                Console.WriteLine("So vua nhap la so chinh phuong: " + n);
+            }
+            else
+            {
+                Console.WriteLine("So vua nhap khong phai la so chinh phuong.");
+            }
+        }
+
+        static bool IsPerfectSquare(int n)
+        {
+            int sqrt = (int)Math.Sqrt(n);
+            return sqrt * sqrt == n;
+        }
+    }
+}
