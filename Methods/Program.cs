c53eabad1abc13f2fd954326cc42f97c6decd416
Thong Nguyn <thongdatng201@gmail.com>
2025-01-17 14:07:20 +0700

    CS03 17/1/25: Method

---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HoTen("Van A", "Nguyen");
+        }
+
+        public static void HoTen(string ten, string ho)
+        {
+            string fullname;
+            fullname = ho + " " + ten;
+
+            Console.WriteLine($"Xin chao { fullname} ");
+
+        }
+
+    }
+}
